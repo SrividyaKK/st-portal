a0d6cc76bd4eaeadbd6bff205c7dc4109842716d
SrividyaKK <SrividyaKK@users.noreply.github.com>
2019-11-03 11:53:02 +0530

    Added initial files

---
+++ app/src/main/java/universe/sk/stportal/MainActivity.java
@@ -0,0 +1,13 @@
+package universe.sk.stportal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
