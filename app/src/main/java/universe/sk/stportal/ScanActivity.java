30a64ea4b7b8be9a8bc2ec6924741835624fdbc2
SrividyaKK <SrividyaKK@users.noreply.github.com>
2019-11-06 15:30:50 +0530

    barcode scanning code added

--- app/src/main/java/universe/sk/stportal/ScanActivity.java
+++ app/src/main/java/universe/sk/stportal/ScanActivity.java
@@ -2,13 +2,45 @@ package universe.sk.stportal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ScanActivity extends AppCompatActivity {
+    private Button scanBtn;
+    private TextView formatText, contentText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        scanBtn = findViewById(R.id.scan_button);
+        formatText = findViewById(R.id.scan_format);
+        contentText = findViewById(R.id.scan_content);
+
+        scanBtn.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.scan_button) {
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
+public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            String scanContent = scanningResult.getContents();
+            String scanFormat - scanningResult.getFormatName();
+            formatText.setText("FORMAT: " + scanFormat);
+            contentText.setText("CONTENT: " + scanContent);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(), "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
