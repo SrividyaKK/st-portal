afa11764f335efd766f68448b20ec89b29e7fe66
SrividyaKK <SrividyaKK@users.noreply.github.com>
2019-11-25 04:54:35 +0530

    individual appln

---
+++ app/src/main/java/universe/sk/stportal/IndividualApplicationActivity.java
@@ -0,0 +1,14 @@
+package universe.sk.stportal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IndividualApplicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_application);
+    }
+}
