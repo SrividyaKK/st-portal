63361ff3c7215918ac3b51a072f647c22b97aed2
Megha K C <meghakc10@gmail.com>
2019-11-24 09:13:44 +0530

    Added veriy for RTO

--- app/src/main/java/universe/sk/stportal/MainActivity.java
+++ app/src/main/java/universe/sk/stportal/MainActivity.java
@@ -1,13 +1,32 @@
 package universe.sk.stportal;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button verify;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        verify = findViewById(R.id.custom_btn);
+        verify.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.custom_btn)
+        {
+            Intent intent = new Intent(this,ScanActivity.class);
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+            //startActivity(intent);
+        }
     }
 }
