eb3f883152856642c55bf75ce166a555b446c2d1
SrividyaKK <SrividyaKK@users.noreply.github.com>
2019-11-25 06:15:53 +0530

    added id for button

--- app/src/main/java/universe/sk/stportal/IndividualApplicationActivity.java
+++ app/src/main/java/universe/sk/stportal/IndividualApplicationActivity.java
@@ -3,9 +3,12 @@ package universe.sk.stportal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class IndividualApplicationActivity extends AppCompatActivity {
-
+    private TextView tvApplicantName;
+    private Button btnVerify;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
