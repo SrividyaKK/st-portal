63ce0131531e03fbf7047837cf1f7cd3ebdbb590
SrividyaKK <SrividyaKK@users.noreply.github.com>
2019-11-25 08:28:06 +0530

    view compl activity

--- app/src/main/java/universe/sk/stportal/ViewComplaintActivity.java
+++ app/src/main/java/universe/sk/stportal/ViewComplaintActivity.java
@@ -3,12 +3,52 @@ package universe.sk.stportal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class ViewComplaintActivity extends AppCompatActivity {
+    ListView lvCompList;
+    ArrayList<Complaint> complaints = new ArrayList<>();
+    private ComplaintAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_complaint);
+
+        lvCompList = findViewById(R.id.lvCompList);
+
+        Set<String> busNums = new LinkedHashSet<>();
+        Set<String> compCounts = new LinkedHashSet<>();
+        Set<String> compMsgs = new LinkedHashSet<>();
+
+        adapter = new ComplaintAdapter(complaints, getApplicationContext());
+        lvCompList.setAdapter(adapter);
+
+        loadData();
+    }
+
+    public void loadData() {
+        // read the contacts from sharedPreferences
+
+        // TODO: LOAD DATA FROM DB
+        Set<String> busNums = new LinkedHashSet<>();
+
+        String name, number;
+        Iterator<String> itrNames = busNums.iterator();
+//        Iterator<String> itrNumbers = contactNumbers.iterator();
+//
+//        complaints.clear();
+//
+//        while (itrNames.hasNext()) {
+//            name = itrNames.next();
+//            number = itrNumbers.next();
+//            contacts.add(new Contact(name, number));
+//        }
+        adapter.notifyDataSetChanged();
     }
 }
