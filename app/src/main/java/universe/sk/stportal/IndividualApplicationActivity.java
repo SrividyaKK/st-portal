fbb3b79335284016971f4795b9ba93d7d157668a
Megha K C <meghakc10@gmail.com>
2019-11-26 07:31:00 +0530

    Added menu to RTO home page

--- app/src/main/java/universe/sk/stportal/IndividualApplicationActivity.java
+++ app/src/main/java/universe/sk/stportal/IndividualApplicationActivity.java
@@ -38,46 +38,46 @@ public class IndividualApplicationActivity extends AppCompatActivity implements 
 
         btnVerify = findViewById(R.id.btn_verify);
 
-        Bundle bundle = getIntent().getExtras(); // get the bundle
-        String id = bundle.getString("id");
-        String name = bundle.getString("name");
-
-        btnVerify.setOnClickListener(this);
-
-        // TODO: FETCH FROM DB
-        JSONObject json = new JSONObject();
-        try {
-            json.put("admno", id);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        Runnable runnable = () -> {
-            OkHttpClient client = new OkHttpClient.Builder().retryOnConnectionFailure(true).build();
-            Request request = new Request.Builder()
-                    .url(getResources().getString(R.string.base_api_url) + "/api/camps/c/" + id)
-                    .header("Content-Type", "application/json")
-                    .build();
-
-            try {
-                Response response = client.newCall(request).execute();
-                JSONObject res = new JSONObject(response.body().string());
-
-                tvApplicantName.setText(res.getString("name"));
-                tvApplicantDOB.setText(res.getString("date"));
-                tvApplicantInstitution.setText(res.getString("institution"));
-                tvApplicantInstLoc.setText(res.getString("inst_loc"));
-                tvApplicantCourse.setText(res.getString("course"));
-                tvApplicantYearOfGrad.setText(res.getString("year"));
-                tvApplicantAddress.setText(res.getString("address"));
-                tvApplicantRoutes.setText(res.getString("routes"));
-            } catch (IOException | JSONException e) {
-                e.printStackTrace();
-            }
-        };
-
-        Thread async = new Thread(runnable);
-        async.start();
+//        Bundle bundle = getIntent().getExtras(); // get the bundle
+//        String id = bundle.getString("id");
+//        String name = bundle.getString("name");
+//
+//        btnVerify.setOnClickListener(this);
+//
+//        // TODO: FETCH FROM DB
+//        JSONObject json = new JSONObject();
+//        try {
+//            json.put("admno", id);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+//        Runnable runnable = () -> {
+//            OkHttpClient client = new OkHttpClient.Builder().retryOnConnectionFailure(true).build();
+//            Request request = new Request.Builder()
+//                    .url(getResources().getString(R.string.base_api_url) + "/api/camps/c/" + id)
+//                    .header("Content-Type", "application/json")
+//                    .build();
+//
+//            try {
+//                Response response = client.newCall(request).execute();
+//                JSONObject res = new JSONObject(response.body().string());
+//
+//                tvApplicantName.setText(res.getString("name"));
+//                tvApplicantDOB.setText(res.getString("date"));
+//                tvApplicantInstitution.setText(res.getString("institution"));
+//                tvApplicantInstLoc.setText(res.getString("inst_loc"));
+//                tvApplicantCourse.setText(res.getString("course"));
+//                tvApplicantYearOfGrad.setText(res.getString("year"));
+//                tvApplicantAddress.setText(res.getString("address"));
+//                tvApplicantRoutes.setText(res.getString("routes"));
+//            } catch (IOException | JSONException e) {
+//                e.printStackTrace();
+//            }
+//        };
+//
+//        Thread async = new Thread(runnable);
+//        async.start();
 
     }
 
