254e7f0d5b006ab86951c5a5f98707dee9d2be3b
Srividya <srividya.krishnakumar99@gmail.com>
2019-11-26 05:38:31 +0530

    completed individual application activity, including the profile pic

--- app/src/main/java/universe/sk/stportal/IndividualApplicationActivity.java
+++ app/src/main/java/universe/sk/stportal/IndividualApplicationActivity.java
@@ -1,16 +1,21 @@
 package universe.sk.stportal;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,8 +23,9 @@ import org.json.JSONObject;
 import java.io.IOException;
 
 public class IndividualApplicationActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView tvApplicantName, tvApplicantDOB, tvApplicantInstitution, tvApplicantInstLoc, tvApplicantCourse, tvApplicantYearOfGrad, tvApplicantAddress,tvApplicantRoutes;
+    private TextView tvApplicantName, tvApplicantEmail, tvApplicantDOB, tvApplicantInstitution, tvApplicantInstLoc, tvApplicantCourse, tvApplicantYearOfGrad, tvApplicantAddress,tvApplicantRoutes;
     private Button btnVerify;
+    CircleImageView ivIndAppProfilePic;
 
     private String id;
     @Override
@@ -29,18 +35,20 @@ public class IndividualApplicationActivity extends AppCompatActivity implements 
 
         tvApplicantName = findViewById(R.id.tvApplicantName);
         tvApplicantDOB = findViewById(R.id.tvApplicantDOB);
+        tvApplicantEmail = findViewById(R.id.tvApplicantEmail);
         tvApplicantInstitution = findViewById(R.id.tvApplicantInstitution);
         tvApplicantInstLoc = findViewById(R.id.tvApplicantInstLoc);
         tvApplicantCourse = findViewById(R.id.tvApplicantCourse);
         tvApplicantYearOfGrad = findViewById(R.id.tvApplicantYearOfGrad);
         tvApplicantAddress = findViewById(R.id.tvApplicantAddress);
         tvApplicantRoutes = findViewById(R.id.tvApplicantRoutes);
+        ivIndAppProfilePic = findViewById(R.id.ivIndAppProfilePic);
 
         btnVerify = findViewById(R.id.btn_verify);
 
         Bundle bundle = getIntent().getExtras(); // get the bundle
         String id = bundle.getString("id");
-        String name = bundle.getString("name");
+//        String name = bundle.getString("name");
 
         btnVerify.setOnClickListener(this);
 
@@ -55,7 +63,7 @@ public class IndividualApplicationActivity extends AppCompatActivity implements 
         Runnable runnable = () -> {
             OkHttpClient client = new OkHttpClient.Builder().retryOnConnectionFailure(true).build();
             Request request = new Request.Builder()
-                    .url(getResources().getString(R.string.base_api_url) + "/api/camps/c/" + id)
+                    .url(getResources().getString(R.string.base_api_url) + "/students/" + id)
                     .header("Content-Type", "application/json")
                     .build();
 
@@ -63,8 +71,16 @@ public class IndividualApplicationActivity extends AppCompatActivity implements 
                 Response response = client.newCall(request).execute();
                 JSONObject res = new JSONObject(response.body().string());
 
-                tvApplicantName.setText(res.getString("name"));
-                tvApplicantDOB.setText(res.getString("date"));
+                String appName = res.getString("firstName") + res.getString("lastName");
+                String imgUrl = res.getString(res.getString("image"));
+                Picasso.get()
+                        .load(imgUrl).fit().centerCrop()
+                        .placeholder(R.drawable.profile_pic_default)
+                        .into(ivIndAppProfilePic);
+
+                tvApplicantName.setText(appName);
+                tvApplicantDOB.setText(res.getString("dob"));
+                tvApplicantEmail.setText(res.getString("email"));
                 tvApplicantInstitution.setText(res.getString("institution"));
                 tvApplicantInstLoc.setText(res.getString("inst_loc"));
                 tvApplicantCourse.setText(res.getString("course"));
