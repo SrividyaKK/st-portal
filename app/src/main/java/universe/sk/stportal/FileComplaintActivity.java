c7d175c0b6f8c571055e2cf75c54e3a2a05185ea
Megha K C <meghakc10@gmail.com>
2019-11-26 08:13:14 +0530

    Changed UI for file complaint

--- app/src/main/java/universe/sk/stportal/FileComplaintActivity.java
+++ app/src/main/java/universe/sk/stportal/FileComplaintActivity.java
@@ -2,6 +2,7 @@ package universe.sk.stportal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class FileComplaintActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class FileComplaintActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_complaint);
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.dark_blue)));
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
     }
 }
